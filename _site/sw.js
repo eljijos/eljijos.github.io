31efe0df002d303ce6aac3211ae69baadaf17136
eljijos <didisable@gmail.com>
2022-02-21 09:55:48 -0800

    ggg2

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220222.0147';
+const cacheName = 'chirpy-20220222.0155';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
